0f4b4dbcf2f2433f30dbfeab4e6a01508e933fca
Omkar <OmkarpandeyKaimur@gmail.com>
2023-09-03 00:33:58 +0530

    'fixed bugs of only one song playing'

--- music-control.js
+++ music-control.js
@@ -1,4 +1,5 @@
 // console.log('hii iam omkar');
+// alert('test the code');
 let music = new Audio('resource/audio/song-13.mp3');
 let songs = [
     {
@@ -117,24 +118,36 @@ let songs = [
         poster: 'resource/eng-img-10.jpeg'
     }
     // {
-    //     id:'e-11',
-    //     songName:'Kabil',
-    //     songName_q:`<p class="upper-line ml">Tum hi ho <br><span class="lower-line">Arjit Singh </span></p>`,
-    //     poster:'resource/hindi-img-9.jpeg'
+    //     id: '20',
+    //     ArtistName: 'Gurru Randhawa',
+    //     songName_q: `<p class="upper-line ">Morni<br><span class="lower-line">Aug 23 2024</span></p>`,
+    //     poster: 'resource/new-release-1.jpeg'
     // },
     // {
-    //     id:'e-12',
-    //     songName:'Kabil',
-    //     songName_q:`<p class="upper-line ml">Ladaki Kharab kar di <br><span class="lower-line">Badshah </span></p>`,
-    //     poster:'resource/hindi-img-9.jpeg'
+    //     id: '21',
+    //     ArtistName: 'Honey singh',
+    //     songName_q: `  <p class="upper-line ml">Blue Eyes<br><span class="lower-line"> Aug 23 2021</span></p>`,
+    //     poster: 'resource/new-release-2.jpeg'
     // },
+    // {
+    //     id: '22',
+    //     ArtistName: 'Meharbaan',
+    //     songName_q: `<p class="upper-line ml">Meharbaan <br><span class="lower-line">Aug 22 2022</span></p>`,
+    //     poster: 'resource/new-release-3.jpeg'
+    // },
+    // {
+    //     id: '23',
+    //     ArtistName: 'Harnoor',
+    //     songName_q: `<p class="upper-line ml">Taare<br><span class="lower-line">Sep 12 2023</span></p>`,
+    //     poster: 'resource/new-release-4.jpeg'
+    // }
 ]
+
 Array.from(document.getElementsByClassName('song-horizontal')).forEach((elm, i) => {
     elm.getElementsByTagName('img')[0].src = songs[i].poster;
     elm.getElementsByTagName('p')[0].innerHTML = songs[i].ArtistName;
     elm.getElementsByTagName('span')[0].innerHTML = "sep 23 2025";
 });
-
 //Audio Controller
 let audioPlay = document.getElementById('audio-play');
 audioPlay.addEventListener('click', () => {
@@ -150,35 +163,48 @@ audioPlay.addEventListener('click', () => {
 })
 
 
-//Audio Index
+//Audio song changer Index by uniqe id
 let songId = 0;
 let audioControlPoster = document.getElementById('poster-audio-controller');
 let audioControlContent = document.getElementById('audioControllerTittle');
 Array.from(document.getElementsByClassName('songList')).forEach((el) => {
-
     el.addEventListener('click', (em) => {
-        songId = em.target.id;
-        songId++;
+        songId = em.currentTarget.id;
+        console.log(songId);
+        //songId++;
         music.src = `resource/audio/song-${songId}.mp3`;
         music.play();
+        //for hindi song
         if (songId < 10) {
             audioControlPoster.src = `resource/hindi-img-${songId}.jpeg`
-        } else {
+        }
+        //for new release song section
+        else if (songId >= 20 && songId <= 23) {
+            music.src = `resource/audio/new-song-${songId}.mp3`;
+            // audioControlPoster.src = `resource/new-release-${songId - 19}.jpeg`
+            music.play();
+        }
+        else {
             audioControlPoster.src = `resource/eng-img-${songId - 9}.jpeg`
         }
         audioPlay.classList.remove('fa-circle-play');
         audioPlay.classList.add('fa-circle-pause');
 
-        let songcontent = songs.filter((els) => {
-            return els.id == songId;
-        });
-        songcontent.forEach(elss => {
-            let { songName_q } = elss;
-            audioControlContent.outerHTML = songName_q;
-        });
+        // let songcontent = songs.filter((els) => {
+        //     return els.id == songId;
+        // });
+        // songcontent.forEach(elss => {
+        //     let { songName_q } = elss;
+        //     audioControlContent.outerHTML = songName_q;
+        // });
+        for (let i = 1; i <= songs.length; i++) {
+          if(songId==songs[i].id)
+            console.log(songs[i].songName_q)
+            audioControlContent.outerHTML = songs[i].songName_q;
+        }
     });
 });
- // new release button control
+// new release button control
 
 
 //time on audio controller
@@ -187,42 +213,42 @@ let endTime = document.getElementById('currentEnd');
 let seek = document.getElementById('seek');
 let bar2 = document.getElementById('bar2');
 let dot = document.getElementById('dot');
-music.addEventListener('timeupdate',()=>{
-   let song_cur =music.currentTime;
-   let song_dur =music.duration;
-   let min1 = Math.floor(song_cur/60);
-   let sec1 = Math.floor(song_cur%60);
-   if (sec1<10) {
-    sec1=`0${sec1}`;
-   }
-   curTime.innerText=`${min1}:${sec1}`;
-   let min2 = Math.floor(song_dur/60);
-   let sec2 = Math.floor(song_dur%60);
-   if (sec2<10) {
-    sec2=`0${sec2}`;
-   }
-   endTime.innerText=`${min2}:${sec2}`;
+music.addEventListener('timeupdate', () => {
+    let song_cur = music.currentTime;
+    let song_dur = music.duration;
+    let min1 = Math.floor(song_cur / 60);
+    let sec1 = Math.floor(song_cur % 60);
+    if (sec1 < 10) {
+        sec1 = `0${sec1}`;
+    }
+    curTime.innerText = `${min1}:${sec1}`;
+    let min2 = Math.floor(song_dur / 60);
+    let sec2 = Math.floor(song_dur % 60);
+    if (sec2 < 10) {
+        sec2 = `0${sec2}`;
+    }
+    endTime.innerText = `${min2}:${sec2}`;
 
-//progress bar
-    let progressbar = parseInt((song_cur / song_dur)*100);
-    seek.value=progressbar;
-    let seekbar=seek.value;
-    bar2.style.width=`${seekbar}%`;
-    dot.style.left=`${seekbar}%`;
+    //progress bar
+    let progressbar = parseInt((song_cur / song_dur) * 100);
+    seek.value = progressbar;
+    let seekbar = seek.value;
+    bar2.style.width = `${seekbar}%`;
+    dot.style.left = `${seekbar}%`;
 });
-seek.addEventListener('change',()=>{
-    music.currentTime=seek.value*(music.duration/100)
+seek.addEventListener('change', () => {
+    music.currentTime = seek.value * (music.duration / 100)
 });
 
 //volume up and down
 let vol = document.getElementById('vol');
 let volBar = document.getElementById('volBar');
 let volDot = document.getElementById('volDot');
-vol.addEventListener('change', ()=>{
+vol.addEventListener('change', () => {
     let Volume = vol.value;
     volBar.style.width = `${Volume}%`;
-    volDot.style.left=`${Volume}%`;
-    music.volume=Volume/100;
+    volDot.style.left = `${Volume}%`;
+    music.volume = Volume / 100;
 })
 function Popup() {
     let x = document.getElementById("popup-box");
